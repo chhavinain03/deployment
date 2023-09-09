d95b2842ed4047ed944899f115d70cd905489ac9
chhavinain03 <122280280+chhavinain03@users.noreply.github.com>
2023-09-09 22:14:03 +0530

    Update earthquake.py

--- earthquake.py
+++ earthquake.py
@@ -21,14 +21,19 @@ with open ('rfc.dat','rb') as f:
 
 
 if clicked ==True:
-    data=[Latitude,Longitude]
-    print(data)
-    st.header("MAGNITUDE")
-    st.subheader(model[0][0])
-    print(model[0][0])
-    st.header("DEPTH")
-    st.subheader(model[0][1])
-    print(model[0][1])
+    if Latitude==0:
+        print("LATITUDE IS NOT DEFINED")
+    elif Longitude==0:
+        print("LONGITUDE IS NOT DEFINED")
+    else:
+        data=[Latitude,Longitude]
+        print(data)
+        st.header("MAGNITUDE")
+        st.subheader(model[0][0])
+        print(model[0][0])
+        st.header("DEPTH")
+        st.subheader(model[0][1])
+        print(model[0][1])
     
   
     
